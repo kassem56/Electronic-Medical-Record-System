e25e0d144179fff3ca6ae903b033f8a2ee87c1e4
kassem56 <kkd5384@g.rit.edu>
2021-12-07 02:49:17 +0400

    creating function main, and insde the function opening the patients csv file to get the info of them and add it separately to lists, then creating objects with each pateint with the lists

--- assignment3.py
+++ assignment3.py
@@ -105,5 +105,44 @@ class Encounter:
                 return self.physician , self.patient ,  self.date, self.disease , self.medication
 
 
+def main():
+                                                    # opening and reading the csv file that contains the info of the patients and put each patient in a list
+            with open("professor_work\patients.csv") as pp: 
+                    reader = csv.reader(pp)
+                    row = list(reader)
 
+                    patient1         = row[1]
+                    patient2         = row[2]
+                    patient3         = row[3]
+                    patient4         = row[4]
+                    patient5         = row[5]
+                    patient6         = row[6]
+                    patient7         = row[7]
+                    patient8         = row[8]
+                    patient9         = row[9]
+                    patient10        = row[10]
+
+                                        #making a list to add all the patients to the list then print them
+
+                    patientss= []
+                    for rows in row:
+                        patientss.append(rows)
+                    
+                    
+
+
+                        # defining an object of class Pateint for each pateint from csv file by the list 
+                    
+                    Patient1_  = Patient(patient1[0],patient1[1],patient1[2],patient1[3])
+                    Patient2_  = Patient(patient2[0],patient2[1],patient2[2],patient2[3])
+                    Patient3_  = Patient(patient3[0],patient3[1],patient3[2],patient3[3])
+                    Patient4_  = Patient(patient4[0],patient4[1],patient4[2],patient4[3])
+                    Patient5_  = Patient(patient5[0],patient5[1],patient5[2],patient5[3])
+                    Patient6_  = Patient(patient6[0],patient6[1],patient6[2],patient6[3])
+                    Patient7_  = Patient(patient7[0],patient7[1],patient7[2],patient7[3])
+                    Patient8_  = Patient(patient8[0],patient8[1],patient8[2],patient8[3])
+                    Patient9_  = Patient(patient9[0],patient9[1],patient9[2],patient9[3])
+                    Patient10_ = Patient(patient10[0],patient10[1],patient10[2],patient10[3])
+
+  
     
